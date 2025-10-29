93aec471f466b17fc0df168b5a9d8458fd140d7b
HTHSMazurczyk <almazurczyk@ctemc.org>
2025-10-29 18:21:03 -0400

    Group data by trial

--- js/results.js
+++ js/results.js
@@ -4,15 +4,15 @@
 // Objective: load and process data from ../data/summative.csv, then plot it
 
 const getData = async () => {
-  const response = await fetch("../data/summative.csv")
+  const response = await fetch('../data/summative.csv')
   const rawData = await response.text()
 
   // Headers on the left, data is row by row
   let data = {}
-  let rows = rawData.split("\n")
-  data.categories = rows[0].split(",").slice(1)
-  data.means = rows[1].split(",").slice(1)
-  data.stds = rows[2].split(",").slice(1) // standard deviations
+  let rows = rawData.split('\n')
+  data.categories = rows[0].split(',').slice(1, 5)
+  data.means = rows[1].split(',').slice(1)
+  data.stds = rows[2].split(',').slice(1) // standard deviations
 
   console.log(data)
   return data
@@ -20,14 +20,19 @@ const getData = async () => {
 
 const createChart = async () => {
   const data = await getData()
-  const canvas = document.getElementById("barChart")
+  const canvas = document.getElementById('barChart')
   const myChart = new Chart(canvas, {
-    type: "bar",
+    type: 'bar',
     data: {
-      labels: data.categories,
+      labels: data.categories.map((x) => x.replace(' S', '')),
       datasets: [
         {
-          data: data.means,
+          label: 'Short Portion',
+          data: data.means.slice(0, 4),
+        },
+        {
+          label: 'Long Portion',
+          data: data.means.slice(4, 8),
         },
       ],
     },
@@ -39,13 +44,13 @@ const createChart = async () => {
         x: {
           title: {
             display: true,
-            text: "Trial",
+            text: 'Trial',
           },
         },
         y: {
           title: {
             display: true,
-            text: "Percent Change Over Five Days (%)",
+            text: 'Percent Change Over Five Days (%)',
           },
           ticks: {
             min: 8,
@@ -58,7 +63,7 @@ const createChart = async () => {
       plugins: {
         title: {
           display: true,
-          text: "The Change in Length of the Cut Planaria After the Trial",
+          text: 'The Change in Length of the Cut Planaria After the Trial',
         },
         legend: {
           display: false,
